348ebc9ea406ccc630b82974f804a7c63ec57dfa
Chantel Lowe <chantellowe17@gmail.com>
2019-08-08 11:27:57 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,6 +24,16 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        :steven => {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        },
+        :nurse => {
+          name: "Nurse",
+          age: "44",
+          attitude: "hot-worried"
+        }
       ]
    }
 }
